a89105aaeb5ed32e61c5e8f02f22d2b5587c4002
bilalkhalidshaikh <muhammadbilalkhalid56@gmail.com>
2025-12-29 00:04:21 +0500

    Added build rules

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -5,6 +5,7 @@ import AuthModal from "../components/AuthModal";
 import OnboardingForm from "../components/OnboardingForm";
 import ChatWidget from "../components/ChatWidget";
 import TextSelectionHandler from "../components/TextSelectionHandler";
+import BrowserOnly from "@docusaurus/BrowserOnly"; // 1. Import this
 
 // Swizzled Root component to add global providers and widgets
 export default function Root({ children }: { children: React.ReactNode }) {
@@ -15,7 +16,13 @@ export default function Root({ children }: { children: React.ReactNode }) {
         {/* Global components */}
         <AuthModal />
         <OnboardingForm />
-        <ChatWidget />
+        {/* <ChatWidget /> */}
+        <BrowserOnly>
+          {() => {
+            const ChatWidget = require("../components/ChatWidget").default; // 2. Lazy load it
+            return <ChatWidget />;
+          }}
+        </BrowserOnly>
         <TextSelectionHandler />
       </ChatProvider>
     </AuthProvider>
